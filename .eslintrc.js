563417dbbd64485512113e51f7b8ff694e9e66af
Coy Evans <coyevans403@gmail.com>
2024-03-19 15:53:05 -0500

    eslint init

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+require("@ariesclark/eslint-config/eslint-patch");
+process.env["ESLINT_PROJECT_ROOT"] = __dirname;
+
+module.exports = {
+	extends: [
+		"@ariesclark/eslint-config",
+		"@ariesclark/eslint-config/next",
+		"@ariesclark/eslint-config/tailwindcss"
+	],
+	root: true,
+	settings: {
+		react: {
+			version: "18"
+		}
+	}
+};
